858b2f97f83f09dd7d801ed90c05503a202e19fa
developer-kamran <developer.kamraniqbal@gmail.com>
2023-09-30 19:30:34 +0500

    final commit

--- store_manager/settings.py
+++ store_manager/settings.py
@@ -50,7 +50,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'Templates'
+            BASE_DIR / 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
